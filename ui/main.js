656d0c5c158ad5ce07c080938205ac814d9a564b
jithinmohan <jithinmohankv55@gmail.com>
2016-09-26 00:21:35 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="EDITED";
+var img = document.getElementById("pic");
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft +'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight,50);
+};
